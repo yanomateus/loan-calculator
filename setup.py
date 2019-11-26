288b638dbb2468bb9a21d22ced88b8944e8e5fd0
yanomateus <yano.mateus@gmail.com>
2019-11-25 21:11:59 -0300

    cleanup

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    'Click>=7.0',
+    'numpy==1.17.4',
+]
 
 setup_requirements = ['pytest-runner', ]
 
