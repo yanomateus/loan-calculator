c0ad81d56d9a055085432ba92f3cc2a7c99b0ee7
yanomateus <yano.mateus@gmail.com>
2019-12-07 13:49:46 -0300

    implement iof tax function over amortization

--- tests/grossup/test_iof_tax_functions.py
+++ tests/grossup/test_iof_tax_functions.py
@@ -1,14 +1,27 @@
 from numpy.testing import assert_almost_equal
 
-from loan_calculator.grossup.iof_tax import amortization_iof, complementary_iof, loan_iof
+from loan_calculator.grossup.iof_tax import (
+    amortization_iof,
+    complementary_iof,
+    loan_iof,
+    amortization_schedule_iof
+)
 
 
 def test_amortization_iof():
-    assert_almost_equal(amortization_iof(100.0, 1.0 / 200, 1), 0.5, decimal=2)
+    assert_almost_equal(amortization_iof(100.0, 1, 1.0 / 200), 0.5, decimal=2)
+
+
+def test_amortization_schedule_iof():
+    assert_almost_equal(
+        amortization_schedule_iof([200.0, 100.0], [1, 2], 1.0 / 200),
+        2.0,
+        decimal=2
+    )
 
 
 def test_amortization_iof_aliquot_bound():
-    assert_almost_equal(amortization_iof(100.0, 1.0 / 10, 1), 1.5, decimal=2)
+    assert_almost_equal(amortization_iof(100.0, 1, 1.0 / 10), 1.5, decimal=2)
 
 
 def test_complementary_iof():
