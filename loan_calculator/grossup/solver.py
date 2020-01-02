6faa2922c9d1f63c6b37f8cb30ba3eafd818565d
yanomateus <yano.mateus@gmail.com>
2020-01-02 18:05:47 -0300

    refactored grossup solver

--- loan_calculator/grossup/solver.py
+++ loan_calculator/grossup/solver.py
@@ -5,47 +5,45 @@ def approximate_grossup(
     net_principal,
     daily_interest_rate,
     return_days,
-    amortization_schedule_cls,
+    amortization_function,
     reduced_tax_function,
     reduced_aliquot,
     complementary_tax_function,
     complementary_aliquot,
     service_fee_function,
     service_fee_aliquot,
+    *args,
+    **kwargs
 ):
-    """Approximate the grossup for given tax and fee functions.
+    """Approximate the grossup given tax and fee functions.
 
     Wrap around scipy.optimize.fsolve to find a zero of a residue function,
     which is evaluated as the difference between a grossed up principal and the
     net principal.
 
-    Let :math:`S_\\circ` be target net principal, :math:`\\mathcal{A}` be an
-    amortization class, :math:`d` the daily interest rate,
-    :math:`n_1,n_2,\\ldots,n_k` the return days, :math:`I^*` a
-    reduced aliquot which quantifies the tax over the amortization schedule,
-    :math:`I^{**}` a complementary aliquot quantifying the tax over the
-    principal and :math:`\\sigma` an aliquot over the principal, quantifying
-    the service fee.
-
     Given
 
-    *   :math:`A = \\mathcal{A}(S, (n_1,\\ldots,n_k), d)` an amortization
-        schedule,
-    *   :math:`f = f_{I^{*}} (A)` a mathematical function evaluating the tax over
-        the amortization schedule,
-    *   :math:`c = c_{I^{**}}(S)` a mathematical function evaluating the tax
-        over the principal,
-    *   :math:`g = g_{\\sigma}(S)` a mathematical function evaluating the
+    *   :math:`a = (a_1,\\ldots,a_k)` a vector of amortizations,
+    *   :math:`r = (n_1,\\ldots,n_k)` a vector of return days,
+    *   :math:`\\alpha` a daily aliquot for the tax over the amortizations,
+    *   :math:`f = f(a,r,\\alpha)` a mathematical function evaluating the tax
+        over the amortizations,
+    *   :math:`s` the principal,
+    *   :math:`\\beta` an aliquot for the tax over the principal,
+    *   :math:`c = c(s,\\beta)` a mathematical function evaluating the tax
+        over the principal,,
+    *   :math:`\\gamma` an aliquot for the service fee over the principal,
+    *   :math:`g = g(s,\\gamma)` a mathematical function evaluating the
         service fee over the principal,
 
     the *residue function* is then defined as
 
     .. math::
 
-        \\Delta_{S_\\circ,A,I^*,I^{**},\\sigma} (S) :=
-        S_\circ - (S - f_{I^*}(A) - c_{I^{**}}(S) - g_\\sigma (S)).
+        \\Delta_{s_\\circ,a,\\alpha,\\beta,\\gamma,r} (s) :=
+        s_\\circ - (s - f(a,r,\\alpha) - c(s,\\beta) - g(s,\\gamma)).
 
-    Thus, the grossed up principal :math:`S` is approximated as a zero of the
+    Thus, the grossed up principal :math:`s` is approximated as a zero of the
     residue function.
 
     Parameters
@@ -54,12 +52,10 @@ def approximate_grossup(
         Is the expected net value for the grossed up principal.
     daily_interest_rate: float, required
         The loan's daily interest rate.
+    amortization_function: list, required
+        List of expected amortizations.
     return_days: list, required
         List of return days, where returns are expected to be performed.
-    amortization_schedule_cls: BaseSchedule, required
-        Amortization schedule class. Must be a subclass of BaseSchedule,
-        since its constructor's interface is assumed, or be a class implementing
-        the same interface.
     reduced_tax_function: Callable, required
         A callable implementing the signature
 
@@ -81,6 +77,10 @@ def approximate_grossup(
             g(principal: float, service_fee_aliquot: float) -> float
     service_fee_aliquot: float, required
         The aliquot to be used by `service_fee_function`.
+    args: list, optional
+        Passed as optional positional arguments to scipy's `fsolve`.
+    kwargs: dict, optional
+        Passed as optional keyword arguments to scipy's `fsolve`.
 
     """
 
@@ -88,7 +88,7 @@ def approximate_grossup(
     p = net_principal
     d = daily_interest_rate
     r_days = return_days
-    a_cls = amortization_schedule_cls
+    a = amortization_function
     r_tax_f = reduced_tax_function
     r_alq = reduced_aliquot
     c_tax_f = complementary_tax_function
@@ -99,8 +99,8 @@ def approximate_grossup(
     def residue_function(s):
 
         return (p -
-                r_tax_f(a_cls(s, d, r_days).amortizations, r_days, r_alq) -
+                r_tax_f(a(s, d, r_days), r_days, r_alq) -
                 c_tax_f(s, c_alq) -
                 fee_f(s, s_alq))
 
-    return fsolve(residue_function, p)[0]
+    return fsolve(residue_function, p, *args, **kwargs)
