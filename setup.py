d4f1b70a7699ab4e58e51feaaac98bbaa6810acd
yanomateus <yano.mateus@gmail.com>
2020-10-15 20:13:52 -0300

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/yanomateus/loan-calculator',
-    version='version='1.0.0'',
+    version='version='1.1.0'',
     zip_safe=False,
 )
